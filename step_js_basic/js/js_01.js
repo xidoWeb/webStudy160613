92c8522f943be20cf44966f33ece941998c45ecc
xidoWeb <erivery2@gmail.com>
2016-08-02 12:53:58 +0900

    js_ 변수 불러오기

---
+++ step_js_basic/js/js_01.js
@@ -0,0 +1,36 @@
+/**
+ecmascript: 유럽 컴퓨터 연합
+javascript: 3 - 5 - 6(ecma 2015, ecma 6) - 7
+- dart, typescript, coffeescript*/
+
+// 변수: 변하는 값, 데이터를 담는 그릇
+
+var box  = 'test';
+var box1 = 9;
+var text = '<p>Lorem ipsum dolor sit amet</p>';
+var tt   = null; 
+var tat;
+
+// document.write(box);
+// document.write(box1);
+// document.write(text);
+
+// alert(box);
+// console.log(box1);
+
+
+/*
+ number    = 숫자;
+ string    = '문자';
+ boolean   = true | false ;
+ null      = 없다 
+ undefined = 정의되지 않은;; 
+ array
+ object
+ function
+_______________________________
+  =   대입하겠다!!!!
+  ==  같다(형분석 x)
+  === 같다(형분석 o , 엄격하게 구분, 현시점에서 사용하는 비교)
+
+ */
